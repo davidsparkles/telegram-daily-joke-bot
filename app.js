b19bfffa277147c7fb7fe0b2cef13abdd9bf54a5
David Hentschel <David Hentschel>
2017-02-28 00:27:13 +0100

    answer to chats and ignore temp files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Promise = require('bluebird')
 const request = require('request-promise')
+const _ = require('lodash')
 
 const token = '321940604:AAF39SaO9LqWDVXHp9xoYlJzCpSQ33B654Y' 
 const baseUrl = `https://api.telegram.org/bot${token}/`
@@ -22,11 +23,42 @@ function getUpdates(baseUrl) {
     body: {
         limit: 10
     },
-    json: true // Automatically stringifies the body to JSON
-};
-	request(options)
-		.then(body => console.log('body: ', body))
-		.catch(error => console.log('error: ', error))
+    json: true
+	}
+	return request(options)
+}
+
+
+function sendMessage(baseUrl, chatId, message) {
+	const url = `${baseUrl}sendMessage`
+	const body = {
+		chat_id: chatId,
+		text: message
+	}
+	var options = {
+    method: 'POST',
+    uri: url,
+    body,
+    json: true
+	}
+	return request(options)
 }
 
 getUpdates(baseUrl)
+	.then(({ ok, result }) => {
+		if (!ok) return Promise.reject()
+		else {
+			console.log(result)
+			const message = 'ty for joining us :)'
+			const chatIds = _.chain(result)
+				.map(({ message }) => message.chat.id)
+				.uniq()
+				.value()
+
+			const messagePromises = _.map(chatIds, chatId => sendMessage(baseUrl, chatId, message))
+
+			return Promise.all(messagePromises)
+		}
+	})
+	.then(() => console.log('done'))
+
