c48b337c9881ad7c040523cdad9dbe1d8e8d75d2
David Hentschel <David Hentschel>
2017-02-28 21:27:46 +0100

    schedule and write back to the recent chats

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const Promise = require('bluebird')
 const request = require('request-promise')
 const _ = require('lodash')
+var schedule = require('node-schedule')
 
 const token = '321940604:AAF39SaO9LqWDVXHp9xoYlJzCpSQ33B654Y' 
 const baseUrl = `https://api.telegram.org/bot${token}/`
+
+const lastMessage = {
+	'8253637': 1488312437
+} // chatId: date
 
 function getMe(baseUrl) {
 	const url = `${baseUrl}getMe`
@@ -44,21 +49,39 @@ function sendMessage(baseUrl, chatId, message) {
 	return request(options)
 }
 
-getUpdates(baseUrl)
-	.then(({ ok, result }) => {
-		if (!ok) return Promise.reject()
-		else {
-			console.log(result)
-			const message = 'ty for joining us :)'
-			const chatIds = _.chain(result)
-				.map(({ message }) => message.chat.id)
-				.uniq()
-				.value()
+function getRecentChats(chatIdsAndDate) {
+	return _.chain(chatIdsAndDate)
+		.filter(({ chatId, date }) => !lastMessage[chatId] || lastMessage[chatId] < date)
+		.each(({ chatId, date }) => {
+			if (!lastMessage[chatId] || lastMessage[chatId] < date) lastMessage[chatId] = date
+		})
+		.map(({ chatId }) => chatId)
+		.value()
+}
 
-			const messagePromises = _.map(chatIds, chatId => sendMessage(baseUrl, chatId, message))
+function answerToMessages(baseUrl, message) {
+	getUpdates(baseUrl)
+		.then(({ ok, result }) => {
+			if (!ok) return Promise.reject()
+			else {
+				const chatIdsAndDate = _.chain(result)
+					.map(({ message }) => ({
+						chatId: message.chat.id,
+						date: message.date
+					}))
+					.orderBy('date', 'desc')
+					.uniqBy(({ chatId }) => chatId)
+					.value()
+				const recentChatIds = getRecentChats(chatIdsAndDate)
+				if (_.size(recentChatIds) > 0) console.log('Answer to: ', recentChatIds.join(', '))
+				const messagePromises = _.map(recentChatIds, chatId => sendMessage(baseUrl, chatId, message))
 
-			return Promise.all(messagePromises)
-		}
-	})
-	.then(() => console.log('done'))
+				return Promise.all(messagePromises)
+			}
+		})
+}
+
+const answerJob = schedule.scheduleJob('*/10 * * * * *', function(){
+  answerToMessages(baseUrl, 'ty for joining us :)')
+})
 
